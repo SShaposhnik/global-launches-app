55ff4940351ae7ca76d56e433d9029db2bc085d6
sshaposhnik <serega24122>
2020-03-30 21:04:06 +0700

    Создана функция

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -5,7 +5,7 @@ import {Spin} from "antd";
 
 const { Header, Content, Footer } = Layout;
 const url = 'https://launchlibrary.net/1.4/launch/next/'
-const launchCount = 3
+const launchCount = 5
 
 
 class LayoutContainer extends Component {
