6912bdcec63b9ebd6d93f688704764b704eed026
sshaposhnik <serega24122>
2020-03-30 21:54:49 +0700

    test commit 2

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -5,7 +5,7 @@ import {Spin} from "antd";
 
 const { Header, Content, Footer } = Layout;
 const url = 'https://launchlibrary.net/1.4/launch/next/'
-const launchCount = 13
+const launchCount = 4
 
 
 class LayoutContainer extends Component {
