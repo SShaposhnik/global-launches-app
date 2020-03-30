86e70cd92013e6db4c8705a92f064f4e50813f7b
sshaposhnik <serega24122>
2020-03-30 21:16:49 +0700

    добавлена функция таймера

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -5,7 +5,7 @@ import {Spin} from "antd";
 
 const { Header, Content, Footer } = Layout;
 const url = 'https://launchlibrary.net/1.4/launch/next/'
-const launchCount = 5
+const launchCount = 13
 
 
 class LayoutContainer extends Component {
