cf09a4712bc4146754a7f1036a142ce94f5d4c8b
sshaposhnik <serega24122>
2020-06-01 18:36:25 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/global-launches-app/precache-manifest.899a0fe4cc7c99633d5f18b573f38bd1.js"
+  "/global-launches-app/precache-manifest.ae1efafe36056a7986672c55176c7c31.js"
 );
 
 self.addEventListener('message', (event) => {
